e56aad17d8ab2994f57365057c9b050cff121e25
Mariano Heller <mheller@inti.gob.ar>
2017-07-14 14:41:50 -0300

    Renombre cronjob

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ var { reComment } = require('./commenter/commenter');
 
 console.log("Running script...\n");
 
-var job = new CronJob({
+var commenterTask = new CronJob({
   cronTime: '0 0 7-23/2 * * *',
   onTick: reComment,
   start: true,
   timeZone: 'America/Argentina/Buenos_Aires'
 });
 
-job.start();
+commenterTask.start();
